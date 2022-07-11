6beb9ca9ab2f30f50e411f4addb552bab07c030d
Harimukti <muktihari554@gmail.com>
2022-07-11 09:28:22 +0700

    first commit

---
+++ view/template/footer.php
@@ -0,0 +1,17 @@
+          
+      </div>
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright Design By @mukti676 
+          
+        </div>
+        <div class="footer-right">
+          Developer : Code Peka @MUKTI676
+          <!-- Create By <a href="#">Code Peka</a> --> 
+        </div>
+      </footer>
+    </div>
+  </div>
+
+</body>
+</html>
